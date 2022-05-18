eebc2cf20e55809a6b996f63b972b6a926735892
Eduardo Ansa <eduardoansa@gmail.com>
2022-05-18 18:56:19 -0400

    actualizando

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -1,6 +1,6 @@
 import { FC, useReducer, useEffect } from 'react';
 import { AuthContext, authReducer } from './';
-import Cookies from 'js-cookie';
+// import Cookies from 'js-cookie';
 import axios from 'axios';
 
 import { happyPetApi } from '../../api';
@@ -18,80 +18,80 @@ const AUTH_INITIAL_STATE: AuthState = {
 }
 
 
-export const AuthProvider:FC = ({ children }) => {
+// export const AuthProvider:FC = ({ children }) => {
 
-    const [state, dispatch] = useReducer( authReducer, AUTH_INITIAL_STATE );
+//     const [state, dispatch] = useReducer( authReducer, AUTH_INITIAL_STATE );
 
-    useEffect(() => {
-        checkToken();
-    }, [])
+//     useEffect(() => {
+//         checkToken();
+//     }, [])
 
-    const checkToken = async() => {
+//     const checkToken = async() => {
 
-        try {
-            const { data } = await happyPetApi.get('/user/validate-token');
-            const { token, user } = data;
-            Cookies.set('token', token );
-            dispatch({ type: '[Auth] - Login', payload: user });
-        } catch (error) {
-            Cookies.remove('token');
-        }
-    }
+//         try {
+//             const { data } = await happyPetApi.get('/user/validate-token');
+//             const { token, user } = data;
+//             Cookies.set('token', token );
+//             dispatch({ type: '[Auth] - Login', payload: user });
+//         } catch (error) {
+//             Cookies.remove('token');
+//         }
+//     }
     
 
 
-    const loginUser = async( email: string, password: string ): Promise<boolean> => {
+//     const loginUser = async( email: string, password: string ): Promise<boolean> => {
 
-        try {
-            const { data } = await happyPetApi.post('/user/login', { email, password });
-            const { token, user } = data;
-            Cookies.set('token', token );
-            dispatch({ type: '[Auth] - Login', payload: user });
-            return true;
-        } catch (error) {
-            return false;
-        }
+//         try {
+//             const { data } = await happyPetApi.post('/user/login', { email, password });
+//             const { token, user } = data;
+//             Cookies.set('token', token );
+//             dispatch({ type: '[Auth] - Login', payload: user });
+//             return true;
+//         } catch (error) {
+//             return false;
+//         }
 
-    }
-
-
-    const registerUser = async( name: string, email: string, password: string ): Promise<{hasError: boolean; message?: string}> => {
-        try {
-            const { data } = await happyPetApi.post('/user/register', { name, email, password });
-            const { token, user } = data;
-            Cookies.set('token', token );
-            dispatch({ type: '[Auth] - Login', payload: user });
-            return {
-                hasError: false
-            }
-
-        } catch (error) {
-            if ( axios.isAxiosError(error) ) {
-                return {
-                    hasError: true,
-                    message: error.response?.data.message
-                }
-            }
-
-            return {
-                hasError: true,
-                message: 'No se pudo crear el usuario - intente de nuevo'
-            }
-        }
-    }
+//     }
 
 
+//     const registerUser = async( name: string, email: string, password: string ): Promise<{hasError: boolean; message?: string}> => {
+//         try {
+//             const { data } = await happyPetApi.post('/user/register', { name, email, password });
+//             const { token, user } = data;
+//             Cookies.set('token', token );
+//             dispatch({ type: '[Auth] - Login', payload: user });
+//             return {
+//                 hasError: false
+//             }
 
-    return (
-        <AuthContext.Provider value={{
-            ...state,
+//         } catch (error) {
+//             if ( axios.isAxiosError(error) ) {
+//                 return {
+//                     hasError: true,
+//                     message: error.response?.data.message
+//                 }
+//             }
 
-            // Methods
-            loginUser,
-            registerUser,
+//             return {
+//                 hasError: true,
+//                 message: 'No se pudo crear el usuario - intente de nuevo'
+//             }
+//         }
+//     }
 
-        }}>
-            { children }
-        </AuthContext.Provider>
-    )
-};
+
+
+//     return (
+//         <AuthContext.Provider value={{
+//             ...state,
+
+//             // Methods
+//             loginUser,
+//             registerUser,
+
+//         }}>
+//             { children }
+//         </AuthContext.Provider>
+//     )
+// };
